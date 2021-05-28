7092000442935f1230c472689721b1c3672f07ff
Ryan <ryanpaynt@uncc.edu>
2021-05-28 14:08:36 -0400

    modified js to output a larger scaled picture when an image is chosen from the gallery

--- assets/tcg-script.js
+++ assets/tcg-script.js
@@ -5,6 +5,7 @@ var picGallery = document.querySelector('#gallery');
 var pokeName = document.querySelector('.name');
 var image = document.querySelector('img');
 var galleryCol = document.querySelector('#cardCol');
+var profPicEl = document.querySelector('#profPic');
 
 var apiSearch = function(pokemon){
     requestUrl = 'https://api.pokemontcg.io/v2/cards?q=name:' + pokemon;
@@ -25,11 +26,9 @@ var renderProfPic = function(card){
     console.log(card.data.length);
     while(i<card.data.length){
         var divEl = document.createElement('div');
-        var refEl = document.createElement('a');
         var imgEl = document.createElement('img');
-        imgEl.setAttribute('src', card.data[i].images.small)
+        imgEl.setAttribute('src', card.data[i].images.small);
         divEl.setAttribute('class', 'pure-u-2-5');
-        refEl.setAttribute
         galleryCol.appendChild(divEl);
         divEl.appendChild(imgEl);
     i += 4;
@@ -38,10 +37,18 @@ var renderProfPic = function(card){
 
 formEl.addEventListener('submit', function(event) {
     event.preventDefault();
-    var element = event.target;
     var searchQuery;
-    if(element.matches('form')){
-        searchQuery = input.value.toLowerCase();
-        apiSearch(searchQuery);
-    }
+    searchQuery = input.value.toLowerCase();
+    apiSearch(searchQuery);
 });
+
+galleryCol.addEventListener('click', function(event){
+    event.preventDefault();
+    var element = event.target;
+    if(element.matches('img')){
+        console.log(element.src);
+        profPicEl.setAttribute('src', element.src);
+        profPicEl.setAttribute('width', '300');
+        profPicEl.setAttribute('height', '400');
+    }
+})
